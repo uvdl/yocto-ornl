4e29f16eaf0b90d17feb166d71e4739ddd3139af
Tyler <waddle77@gmail.com>
2020-09-28 08:04:04 -0700

    Added in ksz init scripts with systemd

---
+++ sources/meta-ornl/recipes-core/ksz-initscripts/files/init-ksz8795.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+### BEGIN INIT INFO
+# Provides:          init-ksz8795
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:
+### END INIT INFO
+DESC=init-ksz8795
+
+SPI=/sys/bus/spi/devices
+SWREG=$SPI/spi0.0/sw/reg
+CONFIG=0xff
+
+case $1 in
+	start)
+		# load module (looks like the first time it fails)
+		modprobe -v spi-ksz8795
+		rmmod spi-ksz8795
+		if modprobe -v spi-ksz8795 ; then
+			# set register to enable KSZ8794 switch
+			log_daemon_msg "echo 0x56=$CONFIG > $SWREG"
+			echo 0x56=$CONFIG > $SWREG
+		fi
+		;;
+	*)
+		;;
+esac
