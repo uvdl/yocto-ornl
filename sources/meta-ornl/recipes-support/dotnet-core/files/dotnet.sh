95ce42f51890ded63164962d5f6900bf7c295a46
Tyler <Tyler>
2022-03-11 10:18:50 -0500

    Added more dotnet exports

--- sources/meta-ornl/recipes-support/dotnet-core/files/dotnet.sh
+++ sources/meta-ornl/recipes-support/dotnet-core/files/dotnet.sh
@@ -4,4 +4,6 @@ if [ -d /usr/share/dotnet/ ]
     then
         export DOTNET_ROOT=/usr/share/dotnet
         export PATH=/usr/share/dotnet:$PATH
+        export PATH=/home/root/.dotnet/tools:$PATH
+        export DOTNET_CLI_TELEMETRY_OPTOUT=1
 fi
